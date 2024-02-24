0c64081f840992bf66b24bf267745398e0d52e47
Fernan007do <barbozaf229@gmail.com>
2024-02-24 14:01:19 -0300

    make new elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './globals.css';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <main className="flex h-screen">
+        <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
+    </main>
   )
 }
 
